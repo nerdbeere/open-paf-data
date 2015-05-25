4cf5d385a7a09931139ea6456e083668bcbcca72
Julian Hollmann <julian.hollmann@gmail.com>
2015-05-25 09:28:12 +0200

    Make document insertion use a lot less db connections

--- src/crawler/handler/abstract-handler.js
+++ src/crawler/handler/abstract-handler.js
@@ -9,41 +9,31 @@ var proto = Handler.prototype;
 
 proto.save = function(newsArr, callback) {
   var callback = callback || function() {};
-  var started = new Date().getTime();
   var table = this._table;
   var db = this._r.db(config.db);
 
-  var done = 0;
-  var docsInserted = 0;
 
-  function isComplete(inserted) {
-    done++;
-    docsInserted += inserted;
-    if (done === newsArr.length) {
-      var finished = new Date().getTime();
-      callback(null, {
-        total: done,
-        inserted: docsInserted,
-        finished: finished,
-        duration: finished - started
+  db.table(table).run(function(err, results) {
+    var identifiers = [];
+    results.forEach(function(news) {
+      identifiers.push(news.identifier);
+    });
+
+    var total = newsArr.length;
+    var docsInserted = 0;
+
+    newsArr.forEach(function(news) {
+      if(!news) return;
+      if(identifiers.indexOf(news.identifier) !== -1) return;
+
+      db.table(table).insert(news).run(function() {
+        docsInserted++;
       });
-    }
-  }
+    });
 
-  newsArr.forEach(function(news) {
-    if (news === false) {
-      return isComplete(0);
-    }
-    news.created = started;
-    db.table(table).filter({identifier: news.identifier}).run(function(err, results) {
-      if (err) throw err;
-      if (results.length === 0) {
-        db.table(table).insert(news).run(function() {
-          isComplete(1);
-        });
-      } else {
-        isComplete(0);
-      }
+    callback(null, {
+      total: total,
+      inserted: docsInserted
     });
   });
 };
